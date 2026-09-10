d0c17bde75f2ab63d4d4c09b418661bf87652608
frikkinfunist <168682074+frikkinfunist@users.noreply.github.com>
2026-09-10 16:43:59 +0530

    Delete humanual-email-replier(2)/humanual-email-replier directory

--- humanual-email-replier(2)/humanual-email-replier/scripts/run_eval.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Run SBERT-based semantic evaluation on N test examples (default 100).
-set -e
-
-N="${1:-100}"
-python -m src.evaluate --config configs/config.yaml --n "$N"
--- humanual-email-replier(2)/humanual-email-replier/scripts/run_pipeline.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# End-to-end: build the SBERT retrieval index, then generate a few sample replies.
-set -e
-
-python -m src.retrieval --config configs/config.yaml
-python -m src.generate --config configs/config.yaml --n 3
